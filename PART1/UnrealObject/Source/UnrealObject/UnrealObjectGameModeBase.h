82ed6e61fed57e79b603b389e6bf3abb3504504e
ppatabox <masin556@gmail.com>
2024-04-25 23:02:43 +0900

    :sparkles: feat [240425] [chungsik] UnrealObject
    언리얼 오브젝트에 대한 이해
    - UObject
    - API 모듈에 대한 설명 등

---
+++ PART1/UnrealObject/Source/UnrealObject/UnrealObjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealObjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALOBJECT_API AUnrealObjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
