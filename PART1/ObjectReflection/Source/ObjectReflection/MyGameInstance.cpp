09776c082d7400bc70f85e75fd481b9abb3c1ad2
ppatabox <masin556@gmail.com>
2024-05-09 23:03:44 +0900

    :notebook: docs [240509] [chungsik] "GameInstance Fix"

--- PART1/ObjectReflection/Source/ObjectReflection/MyGameInstance.cpp
+++ PART1/ObjectReflection/Source/ObjectReflection/MyGameInstance.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MyGameInstance.h"
+#include "Student.h"
+#include "Teacher.h"
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -42,4 +43,24 @@ void UMyGameInstance::Init()
 	UE_LOG(LogTemp, Log, TEXT("학교 이름 기본값 : %s"), *GetClass()->GetDefaultObject<UMyGameInstance>()->SchoolName);
 
 	UE_LOG(LogTemp, Log, TEXT("=============================")); //구분선
+
+	// Object 클래스 불러오기 및 생성
+	UStudent* Student = NewObject<UStudent>();
+	UTeacher* Teacher = NewObject<UTeacher>();
+
+	// 언리얼 오브젝트 객체가 갖고 있는 멤버 변수값을 변경
+	/* Person에서 구현된 GetName(), SetName()을 사용해서 속성정보 변경 */
+	Student->SetName(TEXT("학생1"));
+	UE_LOG(LogTemp, Log, TEXT("새로운 학생 이름 %s"), *Student->GetName());
+
+	// 위처럼 GET SET 함수를 사용할 수도 있지만. 언리얼엔진이 제공하는 리플렉션 시스템을 사용해서도 변경이 가능하다.
+	// 리플렉션 시스템 사용
+
+	FString CurrentTeacherName;
+	FProperty* NameProp = UTeacher::StaticClass()->FindPropertyByName(TEXT("Name"));
+	if (NameProp)
+	{
+		NameProp->GetValue_InContainer(Teacher, &CurrentTeacherName);
+		UE_LOG(LogTemp, Log, TEXT("현재 선생님 이름 %s"), *CurrentTeacherName);
+	}
 }
