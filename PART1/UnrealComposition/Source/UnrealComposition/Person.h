8e0e63e348a9211ba663d2cb5bea056c1a40102e
ppatabox <masin556@gmail.com>
2024-06-07 18:21:09 +0900

    :memo: new [240606] [chungsik] "Unreal Composition"
    Person 코드 추가 열거형

--- PART1/UnrealComposition/Source/UnrealComposition/Person.h
+++ PART1/UnrealComposition/Source/UnrealComposition/Person.h
@@ -17,12 +17,19 @@ class UNREALCOMPOSITION_API UPerson : public UObject
 public:
 	UPerson();
 
-	FORCEINLINE FString& GetName() { return Name; }
+	FORCEINLINE const FString& GetName() const { return Name; } // 레퍼런스 지시자 가 있는 것은 const 를 넣어줘야한다.
 	FORCEINLINE void SetName(const FString& InName) { Name = InName; }
+
+	FORCEINLINE class UCard* GetCard() { return Card;  }
+	FORCEINLINE void SetCard(class UCard* InCard) { Card = InCard; }
 
 protected:
 	//이름속성추가
 	UPROPERTY()
 	FString Name;
+
+	UPROPERTY()
+	//class UCard* Card; //Composition과 관계된 것은 전방선언을 통해 최대한 의존성을 없앨 수 있다. <전통적인 방식>
+	TObjectPtr<class UCard> Card;
 
 };
