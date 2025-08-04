87bda22dd1f73e361dfe4ace12f29e4336d15273
JoshepShin <masin556@gmail.com>
2025-08-05 00:30:45 +0900

    :Sparkles: <250805> <Chungsik> "Unreal Serialization"

---
+++ PART1/UnrealSerialization/Source/UnrealSerialization/MyGameInstance.cpp
@@ -0,0 +1,33 @@
+﻿#include "MyGameInstance.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	//구조체 생성
+	FStudentData RawDataSrc(16, TEXT("ppatabox"));
+
+	//구조체 데이터를 파일로 저장 경로 설정
+	// 프로젝트 디렉토리를 얻어오는 함수 사용해서 Saved 폴더를 묶어서 Saved 폴더 경로를 얻을 수 있다. (프로젝트 폴더에 Saved)
+	const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));
+	UE_LOG(LogTemp, Log, TEXT("저장할 파일 폴더 : %s"), *SavedDir);
+
+	{
+		//파일 이름 지정 
+		const FString RawDataFileNmae(TEXT("RawData.bin"));
+		// 해당 파일에 대한 전체 경로 , 파일에 대한 절대 경로 값을 얻을 수 있다.
+		// 얻으면 폴더가 이상하게 나온다
+		FString RawDataAbsolutePath = FPaths::Combine(*SavedDir, *RawDataAbsolutePath);
+		UE_LOG(LogTemp, Log, TEXT("저장할 파일 전체 경로 : %s"), *RawDataAbsolutePath);
+		
+		// 해당 스트링을 보기 좋게 바꿔준다. 
+		FPaths::MakeStandardFilename(RawDataAbsolutePath);
+		UE_LOG(LogTemp, Log, TEXT("변경할 파일 전체 경로 : %s"), *RawDataAbsolutePath)
+
+	}
+}
---
+++ PART1/UnrealSerialization/Source/UnrealSerialization/MyGameInstance.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+struct FStudentData
+{
+	FStudentData() {}
+	// 인자를 받는 생성자
+	FStudentData(int32 InOder, const FString& InName) : Order(InOder), Name(InName) {}
+
+	int32 Order = -1;
+	FString Name = TEXT("홍길동");
+};
+/**
+ * 
+ */
+UCLASS()
+class UNREALSERIALIZATION_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UMyGameInstance();
+
+	virtual void Init() override;
+};
